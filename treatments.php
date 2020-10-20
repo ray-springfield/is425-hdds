3bb3b6e208185dddcc95d35507cf31ca56055f03
Alexander Wang <alex915979wang@gmail.com>
2020-10-20 15:08:28 -0400

    created basic routing to other main pages

--- treatments.php
+++ treatments.php
@@ -8,16 +8,13 @@
     <!-- Other Imports -->
     <link rel="stylesheet" href="stylesheets/styles.css">
 
-    <title>Home Page</title>
+    <title>Treatments</title>
 </head>
 
 <body class="max-viewport-height">
     <div class="d-flex justify-content-center align-items-center h-100">
         <div>
-            <h1>Welcome, User!</h1>
-            <a href="treatments.php" class="btn btn-outline-primary">Treatments</a>
-            <a href="prediction-form.php" class="btn btn-outline-primary">Predictions</a>
-            <a href="history.php" class="btn btn-outline-primary">History</a>
+            <h1>Treatments Page</h1>
         </div>
     </div>
 </body>
