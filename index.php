846f4ee88ab2c60394fefb732868c9ab79ff9fd9
Alexander Wang <alex915979wang@gmail.com>
2020-10-20 14:24:33 -0400

    created initial directory structure + basic template

--- index.php
+++ index.php
@@ -2,15 +2,24 @@
 <html>
 
 <head>
+    <!-- Bootstrap Imports -->
+    <?php include 'includes/bootstrap-scripts.php'; ?>
+
+    <!-- Other Imports -->
+    <link rel="stylesheet" href="stylesheets/styles.css">
+
     <title>Home Page</title>
 </head>
 
-<body>
-
-<?php
-    echo '<h1>Hello World!</h1>';
-?>
-
+<body class="max-viewport-height">
+    <div class="d-flex justify-content-center align-items-center h-100">
+        <div>
+            <h1>Welcome, User!</h1>
+            <button type="button" class="btn btn-outline-primary">Treatments</button>
+            <button type="button" class="btn btn-outline-primary">Prediction</button>
+            <button type="button" class="btn btn-outline-primary">History</button>
+        </div>
+    </div>
 </body>
 
 </html>
