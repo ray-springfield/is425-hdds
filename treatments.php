17cc3d562e176686400300cb8a89e5d34ff6fea1
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-09 19:17:59 -0500

    added links to treatments page
    Added Bootstrap stretched links to all of the cards in the treatments page.

--- treatments.php
+++ treatments.php
@@ -34,6 +34,8 @@
                             <hr>
                             <p class="card-text">Works in the gut to reduce the amount of fat the body absorbs from
                                 consumed food. Multivitamins should be taken daily to prevent malnutrition.</p>
+                            <a href="https://www.webmd.com/drugs/2/drug-17220/orlistat-oral/details" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -43,6 +45,8 @@
                             <hr>
                             <p class="card-text">Acts on the serotonin receptors in the brain to increase fullness from
                                 smaller amounts of food.</p>
+                            <a href="https://www.aafp.org/afp/2014/1215/p863.html" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -52,6 +56,8 @@
                             <hr>
                             <p class="card-text">A mix of two chemicals, naltrexone and bupropion, to simulataneously
                                 treat depression and smoking, as well as increase fullness from eating.</p>
+                            <a href="https://www.aafp.org/afp/2015/0415/p554.html" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -61,6 +67,8 @@
                             <hr>
                             <p class="card-text">Promotes fullness at smaller quantities of food consumed. Available by
                                 injection only. Also available at a lower dose under a different name, Victoza.</p>
+                            <a href="https://www.saxenda.com/about-saxenda/how-it-works.html" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
@@ -81,6 +89,8 @@
                             <hr>
                             <p class="card-text">Medications that act on the kidneys to help eliminate sodium and water,
                                 reducing blood volume.</p>
+                            <a href="https://www.drugs.com/drug-class/thiazide-diuretics.html#:~:text=Thiazide%20diuretics%20are%20a%20type,functional%20unit%20of%20a%20kidney)."
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -90,6 +100,8 @@
                             <hr>
                             <p class="card-text">Relax blood vessels by blocking the formation of a natural chemical
                                 that narrows blood vessels.</p>
+                            <a href="https://www.mayoclinic.org/diseases-conditions/high-blood-pressure/in-depth/ace-inhibitors/art-20047480#:~:text=Angiotensin%2Dconverting%20enzyme%20(ACE)%20inhibitors%20help%20relax%20your%20veins,your%20heart%20to%20work%20harder."
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -99,6 +111,8 @@
                             <hr>
                             <p class="card-text">Relax blood vessels by blocking the action oaf a natural checmical that
                                 narrows blood vessels.</p>
+                            <a href="https://www.mayoclinic.org/diseases-conditions/high-blood-pressure/in-depth/angiotensin-ii-receptor-blockers/art-20045009"
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -108,6 +122,8 @@
                             <hr>
                             <p class="card-text">Relax the muscles of the blood vessles - some may also slow the heart
                                 rate.</p>
+                            <a href="https://www.mayoclinic.org/diseases-conditions/high-blood-pressure/in-depth/calcium-channel-blockers/art-20047605"
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
@@ -128,6 +144,8 @@
                             <hr>
                             <p class="card-text">Statins block a sustance that the liver needs to make cholesterol,
                                 causing the liver to remove cholesterol from the blood.</p>
+                            <a href="https://www.webmd.com/cholesterol-management/side-effects-of-statin-drugs#:~:text=Statins%20are%20a%20class%20of,about%2025%25%20to%2035%25."
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -137,6 +155,8 @@
                             <hr>
                             <p class="card-text">These resins bind directly to bile acids, forcing the liver to use
                                 excess cholesterol to make more bile acids.</p>
+                            <a href="http://tmedweb.tulane.edu/pharmwiki/doku.php/bile_acid-binding_resins"
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -146,6 +166,8 @@
                             <hr>
                             <p class="card-text">Medications such as ezetimibe (Zetia), which can limit the absorption
                                 of dietary cholesterol in the small intestine.</p>
+                            <a href="https://www.webmd.com/cholesterol-management/qa/what-are-cholesterol-absorption-inhibitors#:~:text=Ezetimibe%20(Zetia)%20works%20to%20lower,cholesterol%20and%20raise%20HDL%20levels."
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -155,6 +177,8 @@
                             <hr>
                             <p class="card-text">A newer class of drugs, known as PCSK9 inhibitors, which help the liver
                                 absorb more LDL cholesterol and lower the amount of cholesterol in circulation.</p>
+                            <a href="https://www.webmd.com/cholesterol-management/pcsk9-inhibitors-treatment"
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
@@ -175,6 +199,8 @@
                             <hr>
                             <p class="card-text">A high glucose level could be indicative of diabetes, which
                                 dramatically increases the risk of heart disease and nerve damage.</p>
+                            <a href="https://www.cdc.gov/diabetes/basics/getting-tested.html" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -184,6 +210,8 @@
                             <hr>
                             <p class="card-text">Blocks enzymes that help digest starches, slowing the rise in blood
                                 sugar. Known as an "alpha-glucosidase inhibitor."</p>
+                            <a href="https://www.rxlist.com/precose-drug.htm" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -193,6 +221,7 @@
                             <hr>
                             <p class="card-text">Increases the amount of glucose leaving the body in urine, and limits
                                 the kidneys from reabsorbing glucose. Known as a "SGLT2 inhibitor."</p>
+                            <a href="https://www.invokana.com/" target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -202,6 +231,8 @@
                             <hr>
                             <p class="card-text">Lowers blood sugar by prompting the pancreas to release more insulin.
                                 This drug may also be called "sulfonylureas."</p>
+                            <a href="https://www.rxlist.com/diabinese-drug.htm" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
@@ -222,6 +253,8 @@
                             <hr>
                             <p class="card-text">Treatments such as skin patches, chewing gum, and tablets, which
                                 provide low levels of nicotine without the toxins in tabacco smoke.</p>
+                            <a href="https://smokefree.gov/tools-tips/how-to-quit/using-nicotine-replacement-therapy"
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -231,6 +264,8 @@
                             <hr>
                             <p class="card-text">A medication that not only reduces cravings for nicotine, but also
                                 blocks the rewarding and reinforcing effects of smoking.</p>
+                            <a href="https://medlineplus.gov/druginfo/meds/a606024.html" target="_blank"
+                                class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -240,6 +275,8 @@
                             <hr>
                             <p class="card-text">A medicine that works on the parts of the brain involved in addictive
                                 behavior, and helps relieve the addiction to smoking.</p>
+                            <a href="https://www.webmd.com/drugs/2/drug-1609/zyban-oral/details" target="_blank"
+                                class="stretched-link"><a>
                         </div>
                     </div>
                     <div class="ml-3 mr-3"></div> <!-- Empty <div> for margin spacing -->
@@ -249,6 +286,8 @@
                             <hr>
                             <p class="card-text">An electronic device that delivers nicotine in a vapour, eliminating
                                 most of the toxic effects of smoking conventional cigarettes.</p>
+                            <a href="https://www.cdc.gov/tobacco/basic_information/e-cigarettes/about-e-cigarettes.html"
+                                target="_blank" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
