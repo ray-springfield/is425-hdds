0fb2c793274bfc09231bca517b1ee2026d068723
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-10 16:49:48 -0500

    added jupyter notebook link
    Added the link of the Jupyter Notebook to the home dashboard page.

--- index.php
+++ index.php
@@ -15,31 +15,47 @@
 </head>
 
 <body class="fade-in">
-    <div class="text-center pt-5 pb-4">
-        <h1 class="display-4">Welcome, User!</h1>
-    </div>
-    <hr>
-    <div class="pt-5 d-flex justify-content-around">
-        <a href="prediction-form.php" class="text-center text-reset text-decoration-none zoom-on-hover">
-            <img src="images/question-mark-icon.png" alt="Question Mark Icon">
-            <br>
-            <h3>Make a Prediction</h3>
-        </a>
-        <a href="history.php" class="text-center text-reset text-decoration-none zoom-on-hover">
-            <img src="images/book-icon.png" alt="Book Icon">
-            <br>
-            <h3>Prediction History</h3>
-        </a>
-        <a href="treatments.php" class="text-center text-reset text-decoration-none zoom-on-hover">
-            <img src="images/medicine-icon.png" alt="Medicine Icon">
-            <br>
-            <h3>Treatments</h3>
-        </a>
-        <a href="diagnosis-methods.php" class="text-center text-reset text-decoration-none zoom-on-hover">
-            <img src="images/stethoscope.png" alt="Diagnosis Icon">
-            <br>
-            <h3>Diagnosis</h3>
-        </a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3"></div> <!-- Empty col for spacing purposes -->
+            <div class="col">
+                <div class="text-center pt-5 pb-4">
+                    <h1 class="display-4">Welcome, User!</h1>
+                </div>
+            </div>
+            <div class="col-3 text-right">
+                <a href="jupyter-notebook.html">Jupyter Notebook</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col pt-5 d-flex justify-content-around">
+                <a href="prediction-form.php" class="text-center text-reset text-decoration-none zoom-on-hover">
+                    <img src="images/question-mark-icon.png" alt="Question Mark Icon">
+                    <br>
+                    <h3>Make a Prediction</h3>
+                </a>
+                <a href="history.php" class="text-center text-reset text-decoration-none zoom-on-hover">
+                    <img src="images/book-icon.png" alt="Book Icon">
+                    <br>
+                    <h3>Prediction History</h3>
+                </a>
+                <a href="treatments.php" class="text-center text-reset text-decoration-none zoom-on-hover">
+                    <img src="images/medicine-icon.png" alt="Medicine Icon">
+                    <br>
+                    <h3>Treatments</h3>
+                </a>
+                <a href="diagnosis-methods.php" class="text-center text-reset text-decoration-none zoom-on-hover">
+                    <img src="images/stethoscope.png" alt="Diagnosis Icon">
+                    <br>
+                    <h3>Diagnosis</h3>
+                </a>
+            </div>
+        </div>
     </div>
 </body>
 
