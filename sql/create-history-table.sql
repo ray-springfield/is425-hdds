6d28c5033a29c54674f8926f0e57001e1068fe82
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-13 16:35:08 -0500

    sql script for creating history table
    Started the creation of the SQL script needed to create the History table for the "History" page.

---
+++ sql/create-history-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE History (
+    historyId INT NOT NULL AUTO_INCREMENT,
+    ageInDays INT NOT NULL,
+    heightInCentimeters INT NOT NULL,
+    genderInCode INT NOT NULL,
+    systolicBloodPressure INT NOT NULL,
+    diastolicBloodPressure INT NOT NULL,
+    cholesteralLevel INT NOT NULL,
+    glucoseLevel INT NOT NULL,
+    smokingInBinary INT NOT NULL,
+    alcoholInBinary INT NOT NULL,
+    physicalInBinary INT NOT NULL,
+    PRIMARY KEY (historyId)
+);
